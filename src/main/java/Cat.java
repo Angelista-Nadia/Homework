5167d46cdcb69104a06c8c01a20dca358669d22a
Надя <angelistta@gmail.com>
2024-02-27 11:32:00 +0300

    Hometask5 про кошек

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,8 +1,18 @@
-
 public class Cat {
-
-    public Cat(String name, int age, String breed) {
-        String newName = name.replace("Murka", "Murr");
-        System.out.println(newName + ": " + age + " лет " + breed);
+    public String name;
+    public int age;
+    public Cat() {
+        System.out.println("Порода: Без породы");
+    }
+    public Cat(String name, int age) {
+        this();
+        this.name = name;
+        this.age = age;
+        System.out.println("Кличка кота: " + name + " | Возрост: " + age);
+    }
+    public void NewName(String name) {
+        String newName = name.replace("Мурка", "Мурр");
+        System.out.println("=====================\n" + name + " сменила кличку на - " + newName);
     }
 }
+
--- src/main/java/CatMain.java
+++ src/main/java/CatMain.java
@@ -1,7 +1,11 @@
+
 public class CatMain {
     public static void main(String[] args) {
-        new Cat("Barsik", 6, "unpedigreed");
-        new Cat("Murka", 2, "unpedigreed");
-        new Cat("Leo", 12, "unpedigreed");
+        new Cat("Барсик", 5);
+        Cat cat2 = new Cat("Мурка", 2);
+        new Cat("Лео", 12);
+
+        Cat cat = new Cat();
+        cat.NewName(cat2.name);
     }
 }
