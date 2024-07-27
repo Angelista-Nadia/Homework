f95acaa140e120da880c9714a2cc0f96f57a30d4
Надя <angelistta@gmail.com>
2024-07-27 21:15:44 +0300

    Hometask 39, 40, 41

---
+++ Hometask39-41/src/test/java/com/example/hometask3941/Hometask3941ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hometask3941;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hometask3941ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
